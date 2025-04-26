c19080944d1b45e05e9d072459da08ea08a46e67
Rxteve93 <111177533+Rxteve93@users.noreply.github.com>
2025-04-26 23:43:43 +0100

    Add files via upload

---
+++ week 3 python user page.py
@@ -0,0 +1,14 @@
+Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+===================== RESTART: C:/Users/odchc/week 3 ass.py ====================
+Enter the original price of the item: 
+===================== RESTART: C:/Users/odchc/week 3 ass.py ====================
+Enter the original price of the item: 2000
+Enter the discount percentage: 20
+The final price after a 20.0% discount is: $1600.00
+>>> 
+===================== RESTART: C:/Users/odchc/week 3 ass.py ====================
+Enter the original price of the item: 50000
+Enter the discount percentage: 2.5
+No discount applied. The final price is: $50000.00
